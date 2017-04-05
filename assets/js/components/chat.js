565c23fc73664dc6fd3ea09972a673ca0eb15c94
mayke <mayke_sargento@hotmail.com>
2017-04-05 18:01:52 -0300

    add renderTemplate function to chat js

--- assets/js/components/chat.js
+++ assets/js/components/chat.js
@@ -8,10 +8,30 @@
       var self = this;
 
       self.bindScroll();
+
+      /** Test renderTemplate()
+
+      var test = self.renderTemplate('chat-message', {
+        avatar: 'https://s-media-cache-ak0.pinimg.com/736x/7b/c7/0d/7bc70d641a5b7986739ed2c768449e65.jpg',
+        name: 'Jason Mraz',
+        text: 'Donec rutrum congue leo eget malesuada. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.',
+        schedule: '10:25'
+      });
+
+      $('.js-chat-messages .viewport .overview').append(test);
+
+      **/
     },
 
     bindScroll: function () {
       $('.js-sidebar-scroll').tinyscrollbar({});
+    },
+
+    renderTemplate: function (key, data) {
+      var liquid = $('#' + key + '-template').html();
+      var template = Liquid.parse(liquid);
+
+      return template.render(data);
     }
   }
 
