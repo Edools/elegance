086e0569aaf31524315b2f2e218be8d937eccf21
mayke <mayke_sargento@hotmail.com>
2017-04-06 10:49:46 -0300

    resolvido problema de scroll no chat

--- assets/js/components/chat.js
+++ assets/js/components/chat.js
@@ -9,22 +9,73 @@
 
       self.bindScroll();
 
-      /** Test renderTemplate()
+      // empty test messages
+      $('#chat-messages').empty();
 
-      var test = self.renderTemplate('chat-message', {
-        avatar: 'https://s-media-cache-ak0.pinimg.com/736x/7b/c7/0d/7bc70d641a5b7986739ed2c768449e65.jpg',
-        name: 'Jason Mraz',
-        text: 'Donec rutrum congue leo eget malesuada. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.',
-        schedule: '10:25'
+      // bind form
+
+      $('#send-messages').on('submit', function (event) {
+        event.preventDefault();
+        const text = $('#text').val();
+        $('#text').val('');
+
+        function pad(n) {
+          return (n < 10) ? ("0" + n) : n;
+        }
+
+        var date = new Date();
+
+        app.chat.messages.push({
+          name: 'Pessoa da Edools',
+          text: text,
+          schedule: pad(date.getHours()) + ':' + pad(date.getMinutes())
+        });
       });
 
-      $('.js-chat-messages .viewport .overview').append(test);
+      const config = {
+        apiKey: 'AIzaSyD2oUy-240XWPEWmGrh6PqJRaoA4lJFN4s',
+        authDomain: 'edools-chat.firebaseapp.com',
+        databaseURL: 'https://edools-chat.firebaseio.com',
+        projectId: 'edools-chat',
+        storageBucket: 'edools-chat.appspot.com',
+        messagingSenderId: '322321995609'
+      };
 
-      **/
+      firebase.initializeApp(config);
+      var database = firebase.database();
+      this.messages = database.ref('messages');
+      this.loadMessages();
     },
 
     bindScroll: function () {
       $('.js-sidebar-scroll').tinyscrollbar({});
+      this.messagesScroll = $('.chat-messages .js-scroll')
+        .tinyscrollbar({})
+        .data("plugin_tinyscrollbar");
+    },
+
+    loadMessages: function () {
+      const setMessage = function (data) {
+        this.insertMessage(data.val());
+
+      }.bind(this);
+
+      this.messages.limitToLast(12).on('child_added', setMessage);
+      this.messages.limitToLast(12).on('child_changed', setMessage);
+    },
+
+    insertMessage: function (data) {
+      var self = this;
+      const $container = $('#chat-messages');
+      var html = self.renderTemplate('chat-message', {
+        avatar: 'https://s-media-cache-ak0.pinimg.com/736x/7b/c7/0d/7bc70d641a5b7986739ed2c768449e65.jpg',
+        name: data.name,
+        text: data.text,
+        schedule: data.schedule
+      });
+
+      $container.append(html);
+      self.messagesScroll.update('bottom');
     },
 
     renderTemplate: function (key, data) {
