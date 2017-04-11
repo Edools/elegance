702e93fcddb9c6db61d1289ca60eca25762bdc3a
williamsbelchior <williamsbelchior@gmail.com>
2017-04-11 18:40:28 -0300

    add item quantity to cart

--- assets/js/components/checkout.js
+++ assets/js/components/checkout.js
@@ -2,6 +2,7 @@
   'use strict';
 
   app.bindCheckout = function () {
+    console.log(0);
     $('#js-terms-of-use-check').change(function () {
       if ($(this).is(":checked") === true) {
         $('.cta-checkout-cart').removeClass('disabled');
@@ -11,5 +12,14 @@
     });
 
     $(document).trigger('app:bind:checkout');
+  },
+
+  app.bindItemQuantity = function () {
+    console.log(1);
+    $('.js-item-quantity').change(function () {
+      console.log(2);
+    });
+
+    $(document).trigger('app:bind:item_quantity');
   };
 })();
