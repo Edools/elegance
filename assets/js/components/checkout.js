bf82e0b4c96a3641ddfe40b5ae0b8e9659486c45
williamsbelchior <williamsbelchior@gmail.com>
2017-04-12 13:17:55 -0300

    remove logs

--- assets/js/components/checkout.js
+++ assets/js/components/checkout.js
@@ -2,7 +2,6 @@
   'use strict';
 
   app.bindCheckout = function () {
-    console.log(0);
     $('#js-terms-of-use-check').change(function () {
       if ($(this).is(":checked") === true) {
         $('.cta-checkout-cart').removeClass('disabled');
@@ -12,14 +11,5 @@
     });
 
     $(document).trigger('app:bind:checkout');
-  },
-
-  app.bindItemQuantity = function () {
-    console.log(1);
-    $('.js-item-quantity').change(function () {
-      console.log(2);
-    });
-
-    $(document).trigger('app:bind:item_quantity');
   };
 })();
