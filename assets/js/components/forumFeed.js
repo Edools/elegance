7b7df692171410cbfc91d723c7ef9afaa30cc780
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2017-09-25 15:40:30 -0300

    [new-forum] add forum feed to lesson page

--- assets/js/components/forumFeed.js
+++ assets/js/components/forumFeed.js
@@ -35,6 +35,10 @@
 
       app.fixForumFeed();
     });
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      app.fixForumFeed();
+    });
   };
 
   app.fixForumFeed = function () {
