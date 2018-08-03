9b356bdcc457795e78899f967a7efb2b1df023ac
Davi <davioliveira.dev@gmail.com>
2018-08-03 18:06:46 -0300

    fix parse variable

--- assets/js/components/strongPassword.js
+++ assets/js/components/strongPassword.js
@@ -27,26 +27,46 @@
 
         return force;
     }
+
+    var formValid = function(event, valid) {
+        var keyCode = event.keyCode || event.which;
+
+        if ( valid && keyCode === 13 ) {
+            return true;
+        }
+        
+        return false;
+    }
   
     app.bindStrongPassword = function () {
         var input = $('.strong--password'),
             form  = input.parents('form:first'),
-            alert = form.find('.alert');
+            alert = form.find('.alert'),
+            valid = false;
 
         $('.strong--password')
             .bind('change focus paste keyup', function () {
                 if ( $(this).val() !== '' && (strength($(this).val()) >= 2) ) {
                     alert.hide();
                     form.find('input[type="submit"]').removeClass('disabled');
+
+                    valid = true;
                 } else {
                     alert.show();
                     form.find('input[type="submit"]').addClass('disabled');
+
+                    valid = false;
                 }
             })
             .bind('blur', function () {
                 alert.hide();
                 form.find('input[type="submit"]').removeClass('disabled');
             });
+
+        // reset form submit
+        form.bind('keypress', function(e) {
+            formValid(e, valid)
+        });
     }
   })();
   
