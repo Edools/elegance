f303100ba8e94b46f0a33bf5bebdeaf0fb43c149
Lucas Charles <lucascharlesz@hotmail.com>
2019-09-10 17:20:51 -0300

    js improves

--- assets/js/components/deadpoolChat.js
+++ assets/js/components/deadpoolChat.js
@@ -12,10 +12,15 @@
     bindDeadpool: function () {
       var src = "http://staging-cdn.eadbox.com/deadpooljs/dist.js";
 
-      var $deadpool = $('.deadpool-chat');
-      var $jwt = $deadpool.data("jwt");
+      this.$deadpool = $('.deadpool-chat');
+      this.$jwt = this.$deadpool.data("jwt");
+      this.$message_list = $('.chat-message-list');
+      this.$chat_room = this.$deadpool.data("chat-room");
+      this.$message_form = $('form#message');
+      this.$message_input = this.$message_form.find('textarea[name=message-body]');
 
-      if ($deadpool.length) {
+      if (this.$deadpool.length) {
+        var that = this;
         loadAsset('Deadpool', src, function() {
           try {
             deadpool.default.on(`connected`, () => {
@@ -24,33 +29,23 @@
 
             deadpool.default.on(`connection_failed`, () => {
               var notification = {
-                "notification": true,
                 "type": "close",
-                "user": {
-                  "name": "Notificação"
-                },
-                "date": new Date(),
-                "message": "Falha ao conectar com o chat!"
+                "message_body": "Falha ao conectar com o chat!"
               }
 
-              app.deadpoolChat.renderNewMessage(notification);
+              app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
             });
 
             deadpool.default.on(`disconnected`, () => {
               var notification = {
-                "notification": true,
                 "type": "exclamation",
-                "user": {
-                  "name": "Notificação"
-                },
-                "date": new Date(),
-                "message": "A conexão com o chat foi encerrada!"
+                "message_body": "A conexão com o chat foi encerrada!"
               }
 
-              app.deadpoolChat.renderNewMessage(notification);
+              app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
             });
 
-            deadpool.default.connect("wss://deadpool.herospark.com/chat-service", $jwt);
+            deadpool.default.connect("wss://deadpool.herospark.com/chat-service", that.$jwt);
           } catch (error) {
               console.log(error);
           }
@@ -62,15 +57,13 @@
       }
     },
     renderNewMessage: function(message) {
-      var $message_list = $('.chat-message-list');
-
       var date = new Date(message.date).toLocaleString();
 
       var $msg_html = $(`
         <div class="row message-item">
           <div class="message-header">
             <div class="col-md-9">
-              ${ message.notification == true ? '<i class="icon-'+message.type+'"> '+message.user.name+'</i>' : '<h5>'+message.user.name+'</h5>' }
+              ${ message.notification == true ? '<i class="icon-'+message.type+'"> Notificação</i>' : '<h5>'+message.user.name+'</h5>' }
             </div>
             <div class="col-md-3 message-timestamp">
               <span class="message-timestamp">${date}</span>
@@ -82,109 +75,81 @@
         </div>
       `)
 
-      $message_list.append($msg_html);
+      this.$message_list.append($msg_html);
     },
     joinRoom: function() {
-      var $deadpool = $('.deadpool-chat');
-      var $chat_room = $deadpool.data("chat-room");
-
       try {
         deadpool.default.on(`joined_room`, () => {
           var notification = {
-            "notification": true,
             "type": "check",
-            "user": {
-              "name": "Notificação"
-            },
-            "date": new Date(),
-            "message": "Conectado ao chat com sucesso!"
+            "message_body": "Conectado ao chat com sucesso!"
           }
 
           app.deadpoolChat.bindMessageEvents();
           app.deadpoolChat.bindPresenceEvent();
 
-          app.deadpoolChat.renderNewMessage(notification);
+          app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
         });
 
         deadpool.default.on(`join_timeout join_crashed join_failed`, () => {
           var notification = {
-            "notification": true,
             "type": "close",
-            "user": {
-              "name": "Notificação"
-            },
-            "date": new Date(),
-            "message": "Falha ao conectar com o chat!"
+            "message_body": "Falha ao conectar com o chat!"
           }
 
-          app.deadpoolChat.renderNewMessage(notification);
+          app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
         });
 
         deadpool.default.on(`closed_room`, () => {
           var notification = {
-            "notification": true,
             "type": "exclamation",
-            "user": {
-              "name": "Notificação"
-            },
-            "date": new Date(),
-            "message": "O chat da transmissão foi encerrado!"
+            "message_body": "O chat da transmissão foi encerrado!"
           }
 
-          app.deadpoolChat.renderNewMessage(notification);
+          app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
         });
 
-        deadpool.default.join($chat_room);
+        deadpool.default.join(this.$chat_room);
       } catch (error) {
           console.log(error);
       }
     },
     bindMessageEvents: function() {
-      var $message_list = $('.chat-message-list');
-
       deadpool.default.on(`new_message`, message => {
-        var should_scroll_to_bottom = $message_list.scrollTop() + $message_list.height() == $message_list.prop("scrollHeight");
+        var should_scroll_to_bottom = this.$message_list.scrollTop() + this.$message_list.height() == this.$message_list.prop("scrollHeight");
         app.deadpoolChat.renderNewMessage(message);
 
         if (should_scroll_to_bottom)
-          $message_list.animate({ scrollTop: $message_list.prop("scrollHeight")}, 0);
+          this.$message_list.animate({ scrollTop: this.$message_list.prop("scrollHeight")}, 0);
       });
 
       deadpool.default.on(`message_sent`, message => {
-        var should_scroll_to_bottom = $message_list.scrollTop() + $message_list.height() == $message_list.prop("scrollHeight");
+        var should_scroll_to_bottom = this.$message_list.scrollTop() + this.$message_list.height() == this.$message_list.prop("scrollHeight");
         app.deadpoolChat.renderNewMessage(message);
 
         if (should_scroll_to_bottom)
-          $message_list.animate({ scrollTop: $message_list.prop("scrollHeight")}, 0);
+          this.$message_list.animate({ scrollTop: this.$message_list.prop("scrollHeight")}, 0);
       });
 
-      var $message_form = $('form#message');
-      var $message_input = $message_form.find('textarea[name=message-body]');
-
-      $message_input.on('keyup', function (e) {
+      this.$message_input.on('keyup', function (e) {
         if (e.keyCode === 13) {
           e.preventDefault();
           app.deadpoolChat.submitMessage();
         }
       });
 
-      $message_form.on('submit', function(e) {
+      this.$message_form.on('submit', function(e) {
         e.preventDefault();
         app.deadpoolChat.submitMessage();
       });
 
       deadpool.default.on(`message_failed message_timeout`, () => {
         var notification = {
-          "notification": true,
           "type": "close",
-          "user": {
-            "name": "Notificação"
-          },
-          "date": new Date(),
-          "message": "Falha ao enviar messagem!"
+          "message_body": "Falha ao enviar messagem!"
         }
 
-        app.deadpoolChat.renderNewMessage(notification);
+        app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
       });
     },
     bindPresenceEvent: function() {
@@ -195,13 +160,20 @@
       });
     },
     submitMessage: function() {
-      var $message_form = $('form#message');
-      var $message_input = $message_form.find('textarea[name=message-body]');
-
-      var message_body = $message_input.val();
-      $message_input.val('');
+      var message_body = this.$message_input.val();
+      this.$message_input.val('');
 
       deadpool.default.sendMessage(message_body);
+    },
+    buildNotification: function(options) {
+      var notification = {
+        "notification": true,
+        "type": options.type,
+        "date": new Date(),
+        "message": options.message_body
+      }
+
+      return notification;
     }
   }
 })();
