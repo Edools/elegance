ee486f9b1338e85a6eaa104071c20efd81907f9e
Célio Barros <celio.barros.filho@gmail.com>
2019-09-12 16:35:20 -0300

    Add webinar message when the webinar is not started

--- assets/js/components/deadpoolChat.js
+++ assets/js/components/deadpoolChat.js
@@ -16,64 +16,86 @@
       this.$jwt = this.$deadpool.data("jwt");
       this.$message_list = $('.chat-message-list');
       this.$chat_room = this.$deadpool.data("chat-room");
+      this.$start_at = moment(this.$deadpool.data("start-at"));
       this.$message_form = $('form#message');
       this.$message_input = this.$message_form.find('textarea[name=message-body]');
 
-      if (this.$deadpool.length) {
-        var that = this;
-        loadAsset('Deadpool', src, function() {
-          try {
-            deadpool.default.on(`connected`, () => {
-              app.deadpoolChat.joinRoom();
-            });
+      var time_to_start = moment.duration(moment().diff(this.$start_at)).asMinutes();
 
-            deadpool.default.on(`connection_failed`, () => {
-              var notification = {
-                "type": "close",
-                "message_body": "Falha ao conectar com o chat!"
-              }
+      if (time_to_start < 0) {
+        if (this.$deadpool.length) {
+          var that = this;
+          loadAsset('Deadpool', src, function() {
+            try {
+              deadpool.default.on(`connected`, () => {
+                app.deadpoolChat.joinRoom();
+              });
+  
+              deadpool.default.on(`connection_failed`, () => {
+                var notification = {
+                  "type": "close",
+                  "message_body": "Falha ao conectar com o chat!"
+                }
+  
+                app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
+              });
+  
+              deadpool.default.on(`disconnected`, () => {
+                var notification = {
+                  "type": "exclamation",
+                  "message_body": "A conexão com o chat foi encerrada!"
+                }
+  
+                app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
+              });
+  
+              deadpool.default.connect("wss://deadpool.herospark.com/chat-service", that.$jwt);
+            } catch (error) {
+                console.log(error);
+            }
+  
+            $(document).trigger('app:deadpool:connected');
+          });
+  
+          $(document).trigger('app:bind:deadpool');
+        }
+      } else {
+        var notification = {
+          "type": "exclamation",
+          "message_body": "Esse Webinar ainda não começou"
+        }
 
-              app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
-            });
-
-            deadpool.default.on(`disconnected`, () => {
-              var notification = {
-                "type": "exclamation",
-                "message_body": "A conexão com o chat foi encerrada!"
-              }
-
-              app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
-            });
-
-            deadpool.default.connect("wss://deadpool.herospark.com/chat-service", that.$jwt);
-          } catch (error) {
-              console.log(error);
-          }
-
-          $(document).trigger('app:deadpool:connected');
-        });
-
-        $(document).trigger('app:bind:deadpool');
+        app.deadpoolChat.renderNewMessage(app.deadpoolChat.buildNotification(notification));
       }
     },
     renderNewMessage: function(message) {
       var date = new Date(message.date).toLocaleString();
 
-      var $msg_html = $(`
-        <div class="row message-item">
-          <div class="message-header">
-            <div class="col-md-9">
-              ${ message.notification == true ? '<i class="icon-'+message.type+'"> Notificação</i>' : '<h5>'+message.user.name+'</h5>' }
+      if (this.time_to_start < 0) {
+        var $msg_html = $(`
+          <div class="row message-item">
+            <div class="message-header">
+              <div class="col-md-9">
+                ${ message.notification == true ? '<i class="icon-'+message.type+'"> Notificação</i>' : '<h5>'+message.user.name+'</h5>' }
+              </div>
+              <div class="col-md-3 message-timestamp">
+                <span class="message-timestamp">${date}</span>
+              </div>
             </div>
-            <div class="col-md-3 message-timestamp">
-              <span class="message-timestamp">${date}</span>
+            <div class="col-md-12 message-body">
+              <p>${message.message}</p>
             </div>
           </div>
-          <div class="col-md-12 message-body">
-            <p>${message.message}</p>
+        `)
+      } else {
+        var $msg_html = $(`
+          <div class="row message-item">
+            <div class="col-md-12 message-header">
+              <i class="icon-${message.type}"> ${message.message}</i>
+            </div>
           </div>
-        </div>
-      `)
+        `)
+      }
 
       this.$message_list.append($msg_html);
     },
