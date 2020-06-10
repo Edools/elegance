215c425486f741c0fdfd376e1d4277f65bc1cdd9
Aline Pereira <alineleeap@gmail.com>
2020-06-10 17:10:07 -0300

    Adicionado customizacao basica do depoimento

--- assets/js/components/carousel.js
+++ assets/js/components/carousel.js
@@ -1,33 +1,97 @@
 (function () {    
-  'use strict';
+    'use strict';
   
-  app.carousel = function(){
-    
-    var homeSlider = $('.home-slider-owl');
-  
-
-    homeSlider.owlCarousel({
-      mouseDrag: false,
-      margin:10,
-      nav:true,
-      autoplay:false,
-      autoplayTimeout:8000,
-      responsive:{
-        0 :{
-          items:1
-        },
-        600:{
-          items:1
-        },
-        1000:{
-          items:1
-        }
-      },
+    app.carousel = function () {
       
+      var homeSlider = $('.home-slider-owl');
 
-    });
+      homeSlider.owlCarousel({
+        mouseDrag: false,
+        margin:10,
+        nav:true,
+        autoplay:false,
+        autoplayTimeout:8000,
+        responsive:{
+          0 :{
+            items:1
+          },
+          600:{
+            items:1
+          },
+          1000:{
+            items:1
+          }
+        },
+        
 
-    $(document).trigger('app:bind:carousel');
-  };  
+      });
 
-})();
+      var homeTestimonials = $('.home-testimonials-carousel');
+
+      homeTestimonials.owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            992:{
+                items:3
+            }
+        },
+        dots: true
+      });
+
+      var teachersSlider = $('.more-items');
+
+      teachersSlider.owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            992:{
+                items:3
+            }
+        },
+        dots: true
+      });
+
+      var ratingsSlider = $('#public-notes');
+
+      ratingsSlider.owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            992:{
+                items:3
+            }
+        },
+        dots: true
+      });
+      
+  
+      $(document).trigger('app:bind:carousel');
+    };  
+  
+  })();
+  
